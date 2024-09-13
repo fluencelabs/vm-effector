8d9d0c384cd02d89d1f7cbf7ddb42d6eecdb7bb2
Maria Kuklina <maria@fluence.one>
2024-09-13 17:03:28 +0200

    add random str

--- src/services/vm_effector/vm_effector/src/main.rs
+++ src/services/vm_effector/vm_effector/src/main.rs
@@ -6,9 +6,17 @@ module_manifest!();
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+const RANDOM_ID: &str = "bafkreiei3vrjgjfbzsayj6jmvebbpxvuafv2ypfzatzm5zuufoh7aaqy5m";
+
 pub fn main() {}
 
 #[marine]
 pub fn version() -> String {
     VERSION.to_string()
+}
+
+
+#[marine]
+pub fn random_id() -> String {
+    RANDOM_ID.to_string()
 }
